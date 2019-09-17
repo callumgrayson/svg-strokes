0748d6edd5578e903fb821b3b802759601549533
callumgrayson <callum@musiio.com>
2019-09-17 14:40:47 +0800

    Cleaned up v2

--- src/V2/V2.js
+++ src/V2/V2.js
@@ -1,34 +1,10 @@
-import React, { useState, useLayoutEffect } from 'react';
-import styled, { css } from 'styled-components';
+import React, { useState } from 'react';
 import './V2.css';
 
 import AlphaSVG2 from './alphaSVG2';
 
 const V2 = () => {
 	const [ refresh, setRefresh ] = useState(1);
-
-	useLayoutEffect(() => {
-		const svgEl = document.querySelectorAll('svg');
-		// console.log('svgEl', svgEl);
-		let paths = {};
-
-		for (let i = 0; i < svgEl.length; i++) {
-			// console.log(
-			// 	'svgEl[i].getAttribute("class")',
-			// 	svgEl[i].getAttribute('class')[3]
-			// );
-			const vb = svgEl[i].getAttribute('viewBox');
-			const letter = svgEl[i].getAttribute('class')[3];
-			const path = svgEl[i].childNodes[0].getAttribute('d');
-			// console.log(`Letter ${i} is ${svgEl[i].getTotalLength()}`);
-			paths[letter] = {
-				viewBox: vb,
-				path: path
-			};
-		}
-
-		console.log('paths', JSON.stringify(paths));
-	});
 
 	const refreshAnimation = () => {
 		setRefresh((prev) => (prev === 0 ? 1 : 0));
